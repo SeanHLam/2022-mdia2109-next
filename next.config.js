bae02b4f462aa2978e1a3161e22a8cb92876e13c
SeanHLam <91505213+SeanHLam@users.noreply.github.com>
2022-03-28 22:25:50 -0700

    resmoced eslist ignore

--- next.config.js
+++ next.config.js
@@ -6,12 +6,6 @@ const nextConfig = {
   }
 }
 
-module.exports = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-}
+
 
 module.exports = nextConfig
